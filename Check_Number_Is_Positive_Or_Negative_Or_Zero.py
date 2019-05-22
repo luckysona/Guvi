8262226835f3d035965e0af22079b82bb08a8704
VinodMeganathan <45658545+VinodMeganathan@users.noreply.github.com>
2019-05-22 17:34:03 +0530

    Create Check_Number_Is_Positive_Or_Negative_Or_Zero.py

---
+++ Check_Number_Is_Positive_Or_Negative_Or_Zero.py
@@ -0,0 +1,10 @@
+''' PROGRAM TO CHECK WHETHER A NUMBER IS POSITIVE OR NEGATIVE OR ZERO '''
+a=int(input("Input : "))
+if(a==0):
+  print("Zero")
+elif(a>0):
+  print("Positive")
+elif(a<0):
+  print("Negative")
+else:
+  print("Invalid Input")
