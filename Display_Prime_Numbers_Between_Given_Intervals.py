d5eab1b52df9d1fd986a34fb044a096727f993ab
VinodMeganathan <45658545+VinodMeganathan@users.noreply.github.com>
2019-05-26 12:12:02 +0530

    Create Display_Prime_Numbers_Between_Given_Intervals.py

---
+++ Display_Prime_Numbers_Between_Given_Intervals.py
@@ -0,0 +1,11 @@
+''' PROGRAM TO DISPLAY PRIME NUMBER BETWEEN GIVEN NUMBERS '''
+a,b=input().split()
+a=int(a)
+b=int(b)
+for num in range(a+1,b):
+   if (num>1):
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
