8b1291716121a20ad2ac4b392526aaceac71af56
VinodMeganathan <45658545+VinodMeganathan@users.noreply.github.com>
2019-05-27 20:58:28 +0530

    Create Display_Armstrong_Numbers_Between_Given_Intervals.py

---
+++ Display_Armstrong_Numbers_Between_Given_Intervals.py
@@ -0,0 +1,14 @@
+''' PROGRAM TO PRINT THE ARMSTRONG NUMBERS BETWEEN TWO INTERVALS '''
+
+a,b=input().split()
+a=int(a)
+b=int(b)
+for i in range(a,b+1):
+  sum=0
+  temp=i
+  while (temp>0):
+    digit=temp%10
+    sum+=digit**3
+    temp//=10
+  if (i==sum):
+    print(i, end=" ")
