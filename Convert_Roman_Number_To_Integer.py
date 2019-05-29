38b0523772f31439192ee4db5108d81c7afc6148
VinodMeganathan <45658545+VinodMeganathan@users.noreply.github.com>
2019-05-29 12:43:39 +0530

    Create Convert_Roman_Number_To_Integer.py

---
+++ Convert_Roman_Number_To_Integer.py
@@ -0,0 +1,5 @@
+""" PROGRAM TO CONVERT ROMAN NUMBER TO INTEGER '''
+a=input()
+b=["","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+if a in b:
+  print(b.index(a))
