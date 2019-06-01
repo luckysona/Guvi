adef783fe5a36b6d8819361413947a6af579810f
VinodMeganathan <45658545+VinodMeganathan@users.noreply.github.com>
2019-06-01 11:50:48 +0530

    Create Form_Largest_Number_From_Given_Numbers.py

---
+++ Form_Largest_Number_From_Given_Numbers.py
@@ -0,0 +1,7 @@
+''' PROGRAM TO PRINT LARGEST POSIBLE VALUE FORMED FROM GIVEN VALUES '''
+a=int(input())
+i_string=input()
+l=i_string.split()
+l.sort()
+for i in range(len(l)-1,-1,-1):
+  print(l[i],end="")
