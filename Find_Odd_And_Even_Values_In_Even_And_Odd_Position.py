b888e58ff06b2187abf7471d6575cc9345afcfcc
VinodMeganathan <45658545+VinodMeganathan@users.noreply.github.com>
2019-06-01 13:13:01 +0530

    Create Find_Odd_And_Even_Values_In_Even_And_Odd_Position.py

---
+++ Find_Odd_And_Even_Values_In_Even_And_Odd_Position.py
@@ -0,0 +1,12 @@
+''' PROGRAM TO FIND ODD VALUES IN EVEN POSITION AND EVEN VALUE IN ODD POSITION '''
+a=int(input())
+i_string=input()
+l=i_string.split()
+for i in range(0,len(l)):
+  if(i%2!=0):
+    if(int(l[i])%2==0):
+      print(l[i],end=" ")
+  elif(i%2==0):
+    if(int(l[i])%2!=0):
+      print(l[i],end=" ")
+    
