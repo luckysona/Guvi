9ba26fb2d77d5fdc6bc2b6f9f38e8b0eafe3a25b
VinodMeganathan <45658545+VinodMeganathan@users.noreply.github.com>
2019-06-11 11:20:19 +0530

    Update Find_Max_Three_Numbers_In_Two_List.py

--- Find_Max_Three_Numbers_In_Two_List.py
+++ Find_Max_Three_Numbers_In_Two_List.py
@@ -1,7 +1,9 @@
-''' PROGRAM TO PRINT MAXIMUM THREE NUMBERS AFTER ADDING TWO LIST NUMBERS '''
+''' PROGRAM TO PRINT MAXIMUM NUMBERS AFTER ADDING TWO LIST NUMBERS '''
 a,b=map(int,input().split())
 a1=list(map(int,input().split()))
 b1=list(map(int,input().split()))
 a1=a1+b1
 a1.sort()
-print(a1[-1],a1[-2],a1[-3])
+a1.reverse()
+for i in range(0,b):
+  print(a1[i],end=" ")
