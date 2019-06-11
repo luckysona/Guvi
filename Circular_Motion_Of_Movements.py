4758a29f371a9ff1ee90ed69fd1419a1e1ebdfa2
VinodMeganathan <45658545+VinodMeganathan@users.noreply.github.com>
2019-06-11 23:04:28 +0530

    Create Circular_Motion_Of_Movements.py

---
+++ Circular_Motion_Of_Movements.py
@@ -0,0 +1,6 @@
+''' PROGRAM TO FIND THE POSITION INITIAL AND AFTER CHANGE IS SAME OR NOT '''
+a=input()
+if (a.find("GLGLGL")!=-1 or a.find("GRGRGR")!=-1 or a.find("GLLG")!=-1 or a.find("GRRG")!=-1):
+    print("yes")
+else:
+    print("no")
