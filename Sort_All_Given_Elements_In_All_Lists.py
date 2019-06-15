0e6cb56a7e4e96efdf633007123f85161fbd743a
VinodMeganathan <45658545+VinodMeganathan@users.noreply.github.com>
2019-06-15 18:51:28 +0530

    Update Sort_All_Given_Elements_In_All_Lists.py

--- Sort_All_Given_Elements_In_All_Lists.py
+++ Sort_All_Given_Elements_In_All_Lists.py
@@ -1,13 +1,9 @@
 ''' PROGRAM TO PRINT THE SORTING OF ALL GIVEN LIST ELEMENTS '''
 a=int(input())
-l=[]
-for i in range(0,a):
-  l.append(input())
-l1=[]
-for i in l:
-  a1=i.split()
-  for j in a1:
-    l1.append(j)
-l1.sort()
-for i in l1:
-  print(i,end=" ")
+l=list(map(int,input().split()))
+for i in range(0,a-1):
+  n=list(map(int,input().split()))
+  for j in n:
+    if j not in l:
+      l.append(j)
+print(*sorted(l))
