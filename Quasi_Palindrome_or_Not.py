21d2902decc8b6b7689bc42031d828022762ae8f
VinodMeganathan <45658545+VinodMeganathan@users.noreply.github.com>
2019-08-16 07:44:09 -0700

    Create Quasi_Palindrome_or_Not.py

---
+++ Quasi_Palindrome_or_Not.py
@@ -0,0 +1,14 @@
+''' CHECK THE GIVEN NUMBER IS QUASI PALINDROMIC NUMBER OR NOT '''
+num=input()
+if num==num[::-1]:
+    print("yes")
+else:
+    value=num.strip("0")
+    if value==value[::-1]:
+        print("yes")
+    else:
+        value=num.lstrip("0")
+        if value==value[::-1]:
+            print("yes")
+        else:
+            print("no")
